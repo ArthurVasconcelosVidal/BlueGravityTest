59aeebee166a89dc293de343f2b8cae7da571749
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2023-11-25 08:22:57 -0300

    Creation of player manager and base player

---
+++ BlueGravityTest/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour{
+    PlayerManager instance;
+
+    [SerializeField] PlayerInput playerInput;
+    [SerializeField] MovementBehavior movementBehavior;
+    [SerializeField] Rigidbody2D playerRigidbody;
+    [SerializeField] GameObject spriteObj;
+
+    public PlayerInput PlayerInput { get { return playerInput; } }
+    public MovementBehavior MovementBehavior { get { return movementBehavior; } }
+    public Rigidbody2D PlayerRigidbody { get { return playerRigidbody; } }
+    public GameObject SpriteObj { get { return spriteObj; } }
+
+    void Awake() {
+        SingletonPattern();
+    }
+
+    void SingletonPattern(){
+        if(instance == null) instance = this;
+        else Destroy(instance);
+    }
+}
