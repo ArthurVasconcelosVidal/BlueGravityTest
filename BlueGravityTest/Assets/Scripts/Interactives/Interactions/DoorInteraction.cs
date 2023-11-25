c660ca7473d8bf1b5eadf98d1a12046aa0aa88fc
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2023-11-25 15:16:59 -0300

    Adding door feedback when opened

--- BlueGravityTest/Assets/Scripts/Interactives/Interactions/DoorInteraction.cs
+++ BlueGravityTest/Assets/Scripts/Interactives/Interactions/DoorInteraction.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class DoorInteraction : InteractiveBehavior{
     [SerializeField] GameObject doorCollider;
+    [SerializeField] GameObject openDoor;
+    [SerializeField] GameObject closedDoor;
+
+    bool closed = true; 
     protected override void InteractionBehavior(){
-        if(VerifyMoyseRaycast())
-            doorCollider.SetActive(!doorCollider.active); 
+        if(VerifyMoyseRaycast()){
+            doorCollider.SetActive(!doorCollider.active);
+            openDoor.SetActive(!openDoor.active);
+            closedDoor.SetActive(!closedDoor.active);
+        }
     }
 }
