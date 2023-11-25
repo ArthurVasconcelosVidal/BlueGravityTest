f9f770076b21d13ee371fec3dbf773a396597ca7
Arthur de Vasconcelos Vidal <58999675+ArthurVasconcelosVidal@users.noreply.github.com>
2023-11-25 08:05:48 -0300

    Input system and axis input

---
+++ BlueGravityTest/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour{
+
+    GameInput inputActions;
+
+    [SerializeField] Vector2 inputAxis = Vector2.zero;
+
+    Vector2 InputAxis { get { return inputAxis; } }
+    
+    private void Awake() {
+        inputActions = new GameInput();
+        inputActions.Enable();
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        InputAxisConfig();
+    }
+
+    void InputAxisConfig(){
+        inputActions.PlayerInput.InputAxis.performed += contextMenu => {
+            inputAxis = contextMenu.ReadValue<Vector2>();
+        };
+        inputActions.PlayerInput.InputAxis.canceled += contextMenu => {
+            inputAxis = Vector2.zero;
+        };
+    }
+}
